cae921c33417d83b47d9f6cdc1fc39e309e12826
Matt Lindsay <matt_epilp@hotmail.com>
2017-02-22 11:35:19 +0000

    Modified gitignore _app folder housing minified and compressed JS and CSS files now included.

---
+++ public/lib/_app/bloodhound-remote.js
@@ -0,0 +1,1 @@
+var addresses=new Bloodhound({datumTokenizer:Bloodhound.tokenizers.obj.whitespace("caption"),queryTokenizer:Bloodhound.tokenizers.whitespace,prefetch:"./js/address.json",remote:{url:"./Api/AddressSearch?search=%QUERY",wildcard:"%QUERY"}});$("#remote .typeahead").typeahead(null,{name:"best-pictures",display:"caption",source:addresses});
